65bef2a0f8a6e90163a5780cfcf8cc8b6dc527c5
Erik Wallace <erikdwallace@gmail.com>
2019-12-11 09:53:54 -0500

    Updates to exercise 26 code on working with the DOM.

--- exercises/20 - The DOM/creating-FINISHED.js
+++ exercises/20 - The DOM/creating-FINISHED.js
@@ -1,56 +1,60 @@
-console.log('it works!');
-
 const myParagraph = document.createElement('p');
-myParagraph.textContent = 'I am a P';
-myParagraph.classList.add('special');
-console.log(myParagraph);
+myParagraph.textContent = 'I am a descriptive paragraph';
+myParagraph.classList.add('fancy');
+// console.log(myParagraph);
 
 const myImage = document.createElement('img');
-myImage.src = 'https://picsum.photos/500';
-myImage.alt = 'Nice photo';
+myImage.src = 'http://picsum.photos/500';
+myImage.alt = 'Stunning photo';
 
 const myDiv = document.createElement('div');
 myDiv.classList.add('wrapper');
-console.log(myDiv);
+// console.log(myDiv);
 
+// appendChild first
 myDiv.appendChild(myParagraph);
 myDiv.appendChild(myImage);
 
+// Add to the page
 document.body.appendChild(myDiv);
 
-// oh shoot! we need to add somethint to the top. like a heading!
+// Note: Use appendChild over append due to poor support
+
+// Add a heading above myDiv with insertAdjacentElement
 const heading = document.createElement('h2');
-heading.textContent = 'Cool Things';
+heading.textContent = 'Award Winning Photographs';
 
-myDiv.insertAdjacentElement('beforebegin', heading);
+myDiv.insertAdjacentElement('afterbegin', heading);
 
-// <ul>
-// <li>One</li>
-// <li>two</li>
-// <li>three</li>
-// <li>four</li>
-// <li>five</li>
-// </ul>
+// Create a list with 5 items
+const myList = document.createElement('ul');
+myList.classList.add('grocery-list');
 
-const list = document.createElement('ul');
-const li = document.createElement('li');
-li.textContent = 'three';
-list.appendChild(li);
+// Add the third item to the ul
+const list3 = document.createElement('li');
+list3.textContent = 'Three';
+myList.appendChild(list3);
 
-document.body.insertAdjacentElement('afterbegin', list);
+// Add the ul to the page
+document.body.insertAdjacentElement('afterbegin', myList);
 
+// Add the rest of the list items 1, 2, 4, 5
 const li5 = document.createElement('li');
 li5.textContent = 'Five';
-list.append(li5);
+myList.append(li5);
 
 const li1 = li5.cloneNode(true);
-li1.textContent = 'one';
-list.insertAdjacentElement('afterbegin', li1);
+li1.textContent = 'One';
+myList.insertAdjacentElement('afterbegin', li1);
 
 const li4 = document.createElement('li');
-li4.textContent = 'four';
+li4.textContent = 'Four';
 li5.insertAdjacentElement('beforebegin', li4);
 
 const li2 = document.createElement('li');
-li2.textContent = 'two';
+li2.textContent = 'Two';
 li1.insertAdjacentElement('afterend', li2);
+
+myList.insertAdjacentElement('afterbegin', li1);
+li2.insertAdjacentElement('afterbegin', li1);
+console.log(myList.outerHTML);
