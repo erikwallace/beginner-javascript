b01f945e88c9d4fa6f587b448537d58f34fea920
Erik Wallace <erikdwallace@gmail.com>
2020-01-07 14:28:20 -0500

    Work with event listeners and event types.

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,36 @@
+// console.log('it works');
+
+// On form submission show a prompt
+
+const wes = document.querySelector('.wes');
+
+wes.addEventListener('click', function(event) {
+  const shouldPageChange = confirm(
+    'This website might be malicious. Do you want to proceed?'
+  );
+  // If cancel is selected from the prompt don't follow the link
+  if (!shouldPageChange) {
+    event.preventDefault();
+  }
+});
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', function(event) {
+  const name = event.currentTarget.name.value;
+  if (name.includes('Chad')) {
+    alert('No Chads are allowed');
+    event.preventDefault();
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+// Other event types besides click
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
+signupForm.name.addEventListener('focus', logEvent);
+signupForm.name.addEventListener('blur', logEvent);
